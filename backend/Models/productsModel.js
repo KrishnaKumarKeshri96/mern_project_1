aeb217d967383c8ed67034b8214897c742993462
krishna <keshri819@gmail.com>
2022-01-09 18:21:51 +0530

    Product Schema Created

--- backend/Models/productsModel.js
+++ backend/Models/productsModel.js
@@ -14,4 +14,55 @@ const productSchema = new mongoose.Schema({
     required: [true, "Please Enter Product Price"],
     maxLength: [6, "price cannot exceed more than 999999"],
   },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  catergory: {
+    type: String,
+    required: [true, "price Enter Product Catergory"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "price Enter Product Stock"],
+    maxLength: [4, "price cannot exceed 9999"],
+  },
+  numOfReview: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+export default mongoose.model("Product", productSchema);
