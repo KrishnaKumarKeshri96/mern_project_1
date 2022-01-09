84634d3197e178fca1fb1ad9d85f9cfa06b640ca
krishna <keshri819@gmail.com>
2022-01-09 15:26:49 +0530

    All Router Working Fine

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 // const express = require("express");
 import express from "express";
+import productRoute from "./routes/productRoute.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1", productRoute);
 
 export { app };
