87d8fb62e2c2dac4a798bdc22db274a7b4cfd7be
krishna <keshri819@gmail.com>
2022-01-27 00:08:38 +0530

    Logging In

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -13,3 +13,31 @@ export const registration = AsyncErrorHandler(async (req, res, next) => {
   const token = user.getJWTToken();
   return res.status(201).json({ success: true, token });
 });
+
+// Login User
+
+export const loginUser = AsyncErrorHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //check If user have given email and password both
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please Enter Email & Password"), 400);
+  }
+
+  const user = await userSchema.findOne({ email: email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email or Password"), 401);
+  }
+  const matched = await user.comparePassword(password);
+  if (!matched) {
+    return next(new ErrorHandler("Invalid Email or Password"), 401);
+  }
+  const token = user.getJWTToken();
+  return res.status(200).json({ success: true, token });
+});
+
+export const getAllUsers = AsyncErrorHandler(async (req, res, next) => {
+  return res.status(200).json({ success: true, user: await userSchema.find() });
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,15 @@
-import { registration } from "../Controllers/UserController.js";
+import {
+  registration,
+  loginUser,
+  getAllUsers,
+} from "../Controllers/UserController.js";
 import express from "express";
 
 const router = express.Router();
 
-router.post("/register", registration);
+router
+  .post("/register", registration)
+  .post("/login", loginUser)
+  .get("/getAllUsers", getAllUsers);
 
 export default router;
