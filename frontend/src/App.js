faf71db40e207669a0a410806b62e3c66bdb4564
krishna <keshri819@gmail.com>
2022-03-04 07:12:06 +0530

    Made Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Header from "./component/layout/Header/Header.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import WebFont from "webfontloader";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+      </Router>
+    </>
   );
 }
 
