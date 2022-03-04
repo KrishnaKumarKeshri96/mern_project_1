ccf48ed8758440819eb36cfb8717960feb3cded1
krishna <keshri819@gmail.com>
2022-03-04 07:20:19 +0530

    Footer Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,22 @@ import { useEffect, useState } from "react";
 import Header from "./component/layout/Header/Header.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import WebFont from "webfontloader";
+import Footer from "./component/layout/Footer/Footer.js";
 
 function App() {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
   return (
     <>
       <Router>
         <Header />
       </Router>
+      <Footer />
     </>
   );
 }
