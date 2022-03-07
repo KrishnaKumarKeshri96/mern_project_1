b61862fd20b370354461587c432a46cde74b4115
krishna <keshri819@gmail.com>
2022-03-07 19:04:46 +0530

    Navbar Icon Working Fine

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,19 +9,26 @@ import ProductDetails from "./component/Product/ProductDetails.js";
 import Search from "./component/Product/Search";
 import Products from "./component/Product/Products.js";
 import LoginSignUp from "./component/User/LoginSignUp";
+import UserOptions from "./component/layout/Header/UserOptions";
+import { useSelector } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/userAction";
 
 function App() {
+  const { isAuthenticated, user } = useSelector((state) => state.user);
   useEffect(() => {
     WebFont.load({
       google: {
         families: ["Roboto", "Droid Sans", "Chilanka"],
       },
     });
+    store.dispatch(loadUser());
   }, []);
   return (
     <>
       <Router>
         <Header />
+        {isAuthenticated && <UserOptions user={user} />}
         <Route exact path="/" component={Home} />
         <Route exact path="/product/:id" component={ProductDetails} />
         <Route exact path="/products" component={Products} />
