15808b720b773768424fdcdfc9ce444a400cc8a4
krishna <keshri819@gmail.com>
2022-03-12 02:34:19 +0530

    Order Confirmation Css Done

--- frontend/src/component/Cart/ConfirmOrder.js
+++ frontend/src/component/Cart/ConfirmOrder.js
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 import CheckoutSteps from "../Cart/CheckoutSteps";
 import { useSelector } from "react-redux";
 import MetaData from "../layout/MetaData";
-
+import "./ConfirmOrder.css";
 import { Link } from "react-router-dom";
 import { Typography } from "@material-ui/core";
 
