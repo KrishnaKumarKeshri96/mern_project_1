6bc08d4e79dff10fe7ed4edf202fad52c0423aca
krishna <keshri819@gmail.com>
2022-03-12 00:39:46 +0530

    Cart Page Designing Finish

--- frontend/src/component/Cart/Cart.js
+++ frontend/src/component/Cart/Cart.js
@@ -1,6 +1,6 @@
-import { Fragment } from "react";
-
-import CartItemCard from "./CartItemCard.js";
+import React, { Fragment } from "react";
+import "./Cart.css";
+import CartItemCard from "./CartItemCard";
 import { useSelector, useDispatch } from "react-redux";
 import { addItemsToCart, removeItemsFromCart } from "../../actions/cartAction";
 import { Typography } from "@material-ui/core";
