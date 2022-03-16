7b27e2ade241f85b60182ab0aee7b8ea4185e6e9
krishna <keshri819@gmail.com>
2022-03-16 23:43:49 +0530

    Added Css For Adding new Product components

--- frontend/src/component/Admin/NewProduct.js
+++ frontend/src/component/Admin/NewProduct.js
@@ -1,5 +1,8 @@
-import { Fragment, useState } from "react";
-
+import React, { Fragment, useEffect, useState } from "react";
+import "./newProduct.css";
+import { useSelector, useDispatch } from "react-redux";
+import { clearErrors, createProduct } from "../../actions/productAction";
+import { useAlert } from "react-alert";
 import { Button } from "@material-ui/core";
 import MetaData from "../layout/MetaData";
 import AccountTreeIcon from "@material-ui/icons/AccountTree";
@@ -8,8 +11,14 @@ import StorageIcon from "@material-ui/icons/Storage";
 import SpellcheckIcon from "@material-ui/icons/Spellcheck";
 import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
 import SideBar from "./Sidebar";
+import { NEW_PRODUCT_RESET } from "../../constants/productConstants";
 
 const NewProduct = ({ history }) => {
+  const dispatch = useDispatch();
+  const alert = useAlert();
+
+  const { loading, error, success } = useSelector((state) => state.newProduct);
+
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
   const [description, setDescription] = useState("");
@@ -28,13 +37,67 @@ const NewProduct = ({ history }) => {
     "SmartPhones",
   ];
 
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+
+    if (success) {
+      alert.success("Product Created Successfully");
+      history.push("/admin/dashboard");
+      dispatch({ type: NEW_PRODUCT_RESET });
+    }
+  }, [dispatch, alert, error, history, success]);
+
+  const createProductSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const myForm = new FormData();
+
+    myForm.set("name", name);
+    myForm.set("price", price);
+    myForm.set("description", description);
+    myForm.set("category", category);
+    myForm.set("Stock", Stock);
+
+    images.forEach((image) => {
+      myForm.append("images", image);
+    });
+    dispatch(createProduct(myForm));
+  };
+
+  const createProductImagesChange = (e) => {
+    const files = Array.from(e.target.files);
+
+    setImages([]);
+    setImagesPreview([]);
+
+    files.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          setImagesPreview((old) => [...old, reader.result]);
+          setImages((old) => [...old, reader.result]);
+        }
+      };
+
+      reader.readAsDataURL(file);
+    });
+  };
+
   return (
     <Fragment>
       <MetaData title="Create Product" />
       <div className="dashboard">
         <SideBar />
         <div className="newProductContainer">
-          <form className="createProductForm" encType="multipart/form-data">
+          <form
+            className="createProductForm"
+            encType="multipart/form-data"
+            onSubmit={createProductSubmitHandler}
+          >
             <h1>Create Product</h1>
 
             <div>
@@ -92,7 +155,13 @@ const NewProduct = ({ history }) => {
             </div>
 
             <div id="createProductFormFile">
-              <input type="file" name="avatar" accept="image/*" multiple />
+              <input
+                type="file"
+                name="avatar"
+                accept="image/*"
+                onChange={createProductImagesChange}
+                multiple
+              />
             </div>
 
             <div id="createProductFormImage">
@@ -101,7 +170,11 @@ const NewProduct = ({ history }) => {
               ))}
             </div>
 
-            <Button id="createProductBtn" type="submit">
+            <Button
+              id="createProductBtn"
+              type="submit"
+              disabled={loading ? true : false}
+            >
               Create
             </Button>
           </form>
