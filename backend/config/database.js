1faa7bf83f00957f56c3af340ac94b0c633def04
krishna <keshri819@gmail.com>
2022-03-19 04:52:42 +0530

    Some Changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 const connectDatabase = () => {
   mongoose
     .connect(
-      "mongodb+srv://krishna:8092772411@cluster0.hdioe.mongodb.net/ecommerce?retryWrites=true&w=majority"
+      "mongodb+srv://krishna:8092772411@cluster0.hdioe.mongodb.net/ecommerce?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
     )
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
